df78e587a5be5ac8a9d09e7865cfdf7888656d55
AZ Productions <66299945+AZProductions@users.noreply.github.com>
2021-10-02 17:59:44 +0200

    Added new MenuItem Layout.

--- SecureVast/MainWindow.xaml.cs
+++ SecureVast/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -13,6 +14,11 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using HandyControl.Controls;
+using HandyControl.Themes;
+using HandyControl;
+using QRCoder;
+using System.Drawing;
+using System.IO;
 
 namespace SecureVast
 {
@@ -24,6 +30,63 @@ namespace SecureVast
         public MainWindow()
         {
             InitializeComponent();
+            menuVersionString.Header = "V" + Assembly.GetExecutingAssembly().GetName().Version.ToString();
+        }
+
+        private void MenuItem_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void menuThemeLight_Checked(object sender, RoutedEventArgs e)
+        {
+            menuThemeDark.IsChecked = false;
+            Theme.SetSkin(this, HandyControl.Data.SkinType.Violet);
+        }
+
+        private void menuThemeDark_Checked(object sender, RoutedEventArgs e)
+        {
+            menuThemeLight.IsChecked = false;
+            Theme.SetSkin(this, HandyControl.Data.SkinType.Dark);
+        }
+
+        private void menuThemeLight_Unchecked(object sender, RoutedEventArgs e)
+        {
+            menuThemeDark.IsChecked = true;
+        }
+
+        private void menuThemeDark_Unchecked(object sender, RoutedEventArgs e)
+        {
+            menuThemeLight.IsChecked = true;
+        }
+
+        private void menuCompare_Click(object sender, RoutedEventArgs e)
+        {
+            HandyControl.Controls.MessageBox.Show("A new version has been detected! Do you want to update?", "Test", MessageBoxButton.YesNo, MessageBoxImage.Question);
+        }
+
+        private void tb_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(tb.Text, QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qrCodeData);
+            Bitmap qrCodeImage = qrCode.GetGraphic(20);
+            img.Source = BitmapToImageSource(qrCodeImage);
+            
+        }
+        private BitmapImage BitmapToImageSource(System.Drawing.Bitmap bitmap)
+        {
+            using (MemoryStream memory = new MemoryStream())
+            {
+                bitmap.Save(memory, System.Drawing.Imaging.ImageFormat.Bmp);
+                memory.Position = 0;
+                BitmapImage bitmapimage = new BitmapImage();
+                bitmapimage.BeginInit();
+                bitmapimage.StreamSource = memory;
+                bitmapimage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapimage.EndInit();
+                return bitmapimage;
+            }
         }
     }
 }
